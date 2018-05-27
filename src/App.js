9cbe78f2ee3beca3662b92cf23143e22c5a2fe6b
milanpro <milan.proell@student.hpi.de>
2018-05-27 11:25:56 +0200

    implement status request

--- src/App.js
+++ src/App.js
@@ -12,16 +12,12 @@ import juliaImg from './images/Julia.png';
 import tonyImg from './images/Tony.jpg';
 
 import plus from './images/plus.svg';
+import axios from "axios/index";
 
 class App extends Component {
     state = {
-        activeIndex: null
-    };
-
-    handleClick = (index) => { this.setState({ activeIndex: index }); };
-
-    render() {
-        const plants = [
+        activeIndex: null,
+        plants: [
             {
                 nickname: 'Carla',
                 genus: 'Monsterra deliciosa',
@@ -54,8 +50,42 @@ class App extends Component {
                 statusLight: 1,
                 statusTemp: 1
             }
-        ];
+        ]
+    };
 
+    componentDidMount(){
+        this.interval = setInterval(this.getStatus,1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    getStatus = () => {
+        axios.get('https://planttra.uber.space/nodejs/api/1/status')
+            .then(res => {
+                const status = res.data;
+                if(status != null){
+                    var i;
+                    for (i = 0; i < status.length; i++) {
+                        const ref = status[i][0];
+                        if(ref.type == 'groundmoisture'){
+                            this.state.plants[0].statusGroundmoisture = ref.statusLevel
+                        }
+                        else if(ref.type == 'temp'){
+                            this.state.plants[0].statusTemp = ref.statusLevel
+                        }
+                        else if(ref.type == 'light'){
+                            this.state.plants[0].statusLight = ref.statusLevel
+                        }
+                    }
+                    }
+            })
+    };
+
+
+    handleClick = (index) => { this.setState({ activeIndex: index }); };
+
+    render() {
         return (
             <div className="App">
                 <header className="App-header">
@@ -67,7 +97,7 @@ class App extends Component {
                     <Row>
                         <Col xs={{ size: 6, offset: this.state.activeIndex === null ? 3 : 0 }} >
                             <Row>
-                                { plants.map((plant, i) => {
+                                { this.state.plants.map((plant, i) => {
                                     return (
                                         <Col xs="4">
                                             <PlantCard key={ plant.nickname }
@@ -88,12 +118,12 @@ class App extends Component {
                         </Col>
                         <Col xs="6" className={ this.state.activeIndex === null ? 'd-none' : '' } >
                             {this.state.activeIndex !== null &&
-                                <PlantDashboard nickname={plants[this.state.activeIndex].nickname}
-                                                genus={plants[this.state.activeIndex].genus}
-                                                image={plants[this.state.activeIndex].image}
-                                                statusGroundmoisture={plants[this.state.activeIndex].statusGroundmoisture}
-                                                statusLight={plants[this.state.activeIndex].statusLight}
-                                                statusTemp={plants[this.state.activeIndex].statusTemp}/>
+                                <PlantDashboard nickname={this.state.plants[this.state.activeIndex].nickname}
+                                                genus={this.state.plants[this.state.activeIndex].genus}
+                                                image={this.state.plants[this.state.activeIndex].image}
+                                                statusGroundmoisture={this.state.plants[this.state.activeIndex].statusGroundmoisture}
+                                                statusLight={this.state.plants[this.state.activeIndex].statusLight}
+                                                statusTemp={this.state.plants[this.state.activeIndex].statusTemp}/>
                             }
                         </Col>
                     </Row>
--- src/PlantDiagram.js
+++ src/PlantDiagram.js
@@ -5,23 +5,23 @@ import axios from 'axios';
 
 class PlantDiagram extends Component {
      state = {
-         waterData: [],
+         data: [],
          unit: ""
      };
 
-    getWaterData = () => {
+    getData = () => {
         axios.get('https://planttra.uber.space/nodejs/api/1/graphData?type='+this.props.type)
             .then(res => {
-                const waterData = res.data;
+                const data = res.data;
                 const unit = res.data[0].unit;
-                this.setState({ waterData })
+                this.setState({ data })
                 this.setState({ unit })
             })
     }
 
     render () {
         return (
-            <LineChart width={400} height={120} margin={{top: 20, right: 0, bottom: 0, left: 20}} data={this.state.waterData}>
+            <LineChart width={400} height={120} margin={{top: 20, right: 0, bottom: 0, left: 20}} data={this.state.data}>
                 <YAxis type="number" domain={['dataMin - 3', 'dataMax + 3']} unit={this.state.unit} />
                 <Line type="monotone" dataKey="value" stroke="#8884d8" />
                 <Tooltip />
@@ -30,7 +30,7 @@ class PlantDiagram extends Component {
         );
     }
     componentDidMount(){
-        this.interval = setInterval(this.getWaterData,1000);
+        this.interval = setInterval(this.getData,1000);
     }
     componentWillUnmount() {
         clearInterval(this.interval);
