18b44c9eac6c91f853926599b3e2c920fa52c238
bakoe <ba.koe96@gmail.com>
2018-05-27 03:20:32 +0200

    Add sample implementation of state-based plant card selection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    activeIndex: null
+  };
+
+  handleClick = (index) => { this.setState({ activeIndex: index }); };
+
   render() {
+    const plants = [
+        { name: 'Carla' },
+        { name: 'Patrick' },
+        { name: 'Julia' },
+        { name: 'Tony' }
+    ];
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Plant Tracker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
+            <PlantCard name="Carla" index={0} isActive={ this.state.activeIndex===0 } onClick={ this.handleClick } />
+            <PlantCard name="Patrick" index={1} isActive={ this.state.activeIndex===1 } onClick={ this.handleClick }/>
+            <PlantCard name="Julia" index={2} isActive={ this.state.activeIndex===2 } onClick={ this.handleClick }/>
+
       </div>
     );
   }
+}
+
+class PlantList extends Component {
+    render() {
+        return <div></div>
+    }
+}
+
+class PlantCard extends Component {
+    handleClick = () => { this.props.onClick(this.props.index); };
+
+    render() {
+        return <div className={ this.props.isActive ? 'active' : 'test' } onClick={ this.handleClick }>
+            <span>{ this.props.name }</span>
+        </div>
+    }
 }
 
 export default App;
